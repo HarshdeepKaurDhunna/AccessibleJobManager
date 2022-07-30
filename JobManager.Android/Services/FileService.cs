4acecda39f929c7acc39600785a21a4cc346ccc1
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-07-29 23:46:37 -0400

    Firbase Storage for image

---
+++ JobManager.Android/Services/FileService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Android.App;
+using JobManager.Droid.Services;
+using JobManager.Services;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileService))]
+namespace JobManager.Droid.Services
+{
+    
+    public class FileService : IFileService
+    {
+        public void createFile(string textMessage)
+        {
+            var destination = Path.Combine(getRootPath(), "TextFile.txt");
+            File.WriteAllText(destination, textMessage);
+        }
+
+        public string getRootPath()
+        {
+            return Android.App.Application.Context.GetExternalFilesDir(null).ToString();
+        }
+    }
+}
--- JobManager/Views/WelcomePage.xaml.cs
+++ JobManager/Views/WelcomePage.xaml.cs
@@ -1,20 +1,80 @@
-﻿using System;
+﻿using Firebase.Storage;
+using JobManager.Services;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Xaml;
 
 namespace JobManager.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class WelcomePage : ContentPage
+    public partial class WelcomePage : TabbedPage
     {
         public WelcomePage()
         {
             InitializeComponent();
         }
+        public String downloadlinkVal;
+        public String downloadlinkFileVal;
+        public ImageSource Source { get; set; }
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var photo = await Xamarin.Essentials.MediaPicker.PickPhotoAsync();
+
+            if (photo == null) return;
+           
+
+            var task = new FirebaseStorage("andriod--auth.appspot.com",
+                new FirebaseStorageOptions {
+                      ThrowOnCancel =  true
+                })
+                .Child("image")
+                .Child(photo.FileName).PutAsync(await photo.OpenReadAsync());
+            task.Progress.ProgressChanged += (s, args) =>
+            {
+                progressBar.Progress = args.Percentage;
+            };
+
+
+           {
+
+            };
+            downloadlinkVal = await task;
+           
+        }
+
+        public void button_download_image_Clicked(object sender, EventArgs e)
+        {
+            backgroundImage.Source = downloadlinkVal;
+            backgroundImage.Source = new UriImageSource
+            {
+                Uri = new Uri(downloadlinkVal),
+                CachingEnabled = true,
+                CacheValidity = new TimeSpan(5, 0, 0, 0)
+            };
+        }
+
+        async public void saveTextFile(object sender, EventArgs e)
+        {
+            var message = TextMessage.Text;
+            var service = DependencyService.Get<IFileService>();
+            service.createFile(message);
+            var file = await Xamarin.Essentials.FilePicker.PickAsync();
+
+            if (file == null) return;
+
+
+            var task = new FirebaseStorage("andriod--auth.appspot.com");
+            
+        }
+
+
     }
 }
